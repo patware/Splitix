6881d21e84703b9e67fc1b040f4389a905d8869b
patware <patware@hotmail.com>
2014-04-17 17:04:00 -0400

    NuGet update of the References

--- Splitix/ViewModel/MainViewModel.cs
+++ Splitix/ViewModel/MainViewModel.cs
@@ -1,236 +1,34 @@
-﻿using GalaSoft.MvvmLight;
-using Splitix.Model;
+using GalaSoft.MvvmLight;
 
 namespace Splitix.ViewModel
 {
     /// <summary>
     /// This class contains properties that the main View can data bind to.
     /// <para>
+    /// Use the <strong>mvvminpc</strong> snippet to add bindable properties to this ViewModel.
+    /// </para>
+    /// <para>
+    /// You can also use Blend to data bind with the tool's support.
+    /// </para>
+    /// <para>
     /// See http://www.galasoft.ch/mvvm
     /// </para>
     /// </summary>
     public class MainViewModel : ViewModelBase
     {
-        private readonly IDataService _dataService;
-        private readonly Service.IInputParser _parser;
-
         /// <summary>
         /// Initializes a new instance of the MainViewModel class.
         /// </summary>
-        public MainViewModel(IDataService dataService, Service.IInputParser parser)
+        public MainViewModel()
         {
-            
-            _dataService = dataService;
-            _parser = parser;
-
-            _dataService.GetData(
-                (item, error) =>
-                {
-                    if (error != null)
-                    {
-                        // Report error here
-                        return;
-                    }
-
-                    Input = item.InitialInput;
-                    this.NumberOfItems = item.InitialNumberOfItems;
-                    
-                });
-
-            this.PropertyChanged += new System.ComponentModel.PropertyChangedEventHandler(MainViewModel_PropertyChanged);
-
-            
+            ////if (IsInDesignMode)
+            ////{
+            ////    // Code runs in Blend --> create design time data.
+            ////}
+            ////else
+            ////{
+            ////    // Code runs "for real"
+            ////}
         }
-
-        void MainViewModel_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
-        {
-            switch (e.PropertyName)
-            {
-                case OutputPropertyName:
-                    break;
-
-                default:
-                    doParse();
-                    break;
-            }
-        }
-
-        private void doParse()
-        {
-            var axis = (Splitix.Service.Axis)this.SelectedAxisIndex;
-            var orientation = (Splitix.Service.Orientation)this.SelectedFillByIndex;
-
-            this.Output = _parser.Parse(this.Input, this.NumberOfItems, axis, orientation);
-        }
-
-        ////public override void Cleanup()
-        ////{
-        ////    // Clean up if needed
-
-        ////    base.Cleanup();
-        ////}
-
-        #region Properties
-        #region Input
-        /// <summary>
-        /// The <see cref="Input" /> property's name.
-        /// </summary>
-        public const string InputPropertyName = "Input";
-
-        private string _input;
-
-        /// <summary>
-        /// Sets and gets the Input property.
-        /// Changes to that property's value raise the PropertyChanged event. 
-        /// </summary>
-        public string Input
-        {
-            get
-            {
-                return _input;
-            }
-
-            set
-            {
-                if (_input == value)
-                {
-                    return;
-                }
-
-                RaisePropertyChanging(InputPropertyName);
-                _input = value;
-                RaisePropertyChanged(InputPropertyName);
-            }
-        }
-        #endregion
-        #region NumberOfItems
-        /// <summary>
-        /// The <see cref="NumberOfItems" /> property's name.
-        /// </summary>
-        public const string NumberOfItemsPropertyName = "NumberOfItems";
-
-        private int _numberOfItems;
-
-        /// <summary>
-        /// Sets and gets the NumberOfItems property.
-        /// Changes to that property's value raise the PropertyChanged event. 
-        /// </summary>
-        public int NumberOfItems
-        {
-            get
-            {
-                return _numberOfItems;
-            }
-
-            set
-            {
-                if (_numberOfItems == value)
-                {
-                    return;
-                }
-
-                RaisePropertyChanging(NumberOfItemsPropertyName);
-                _numberOfItems = value;
-                RaisePropertyChanged(NumberOfItemsPropertyName);
-            }
-        }
-        #endregion
-        #region SelectedAxisIndex
-        /// <summary>
-        /// The <see cref="SelectedAxisIndex" /> property's name.
-        /// </summary>
-        public const string SelectedAxisIndexPropertyName = "SelectedAxisIndex";
-
-        private int _selectedAxisIndex;
-
-        /// <summary>
-        /// Sets and gets the SelectedAxisIndex property.
-        /// Changes to that property's value raise the PropertyChanged event. 
-        /// </summary>
-        public int SelectedAxisIndex
-        {
-            get
-            {
-                return _selectedAxisIndex;
-            }
-
-            set
-            {
-                if (_selectedAxisIndex == value)
-                {
-                    return;
-                }
-
-                RaisePropertyChanging(SelectedAxisIndexPropertyName);
-                _selectedAxisIndex = value;
-                RaisePropertyChanged(SelectedAxisIndexPropertyName);
-            }
-        }
-        #endregion
-        #region SelectedFillByIndex
-        /// <summary>
-        /// The <see cref="SelectedFillByIndex" /> property's name.
-        /// </summary>
-        public const string SelectedFillByIndexPropertyName = "SelectedFillByIndex";
-
-        private int _selectedFillByIndex;
-
-        /// <summary>
-        /// Sets and gets the SelectedFillByIndex property.
-        /// Changes to that property's value raise the PropertyChanged event. 
-        /// </summary>
-        public int SelectedFillByIndex
-        {
-            get
-            {
-                return _selectedFillByIndex;
-            }
-
-            set
-            {
-                if (_selectedFillByIndex == value)
-                {
-                    return;
-                }
-
-                RaisePropertyChanging(SelectedFillByIndexPropertyName);
-                _selectedFillByIndex = value;
-                RaisePropertyChanged(SelectedFillByIndexPropertyName);
-            }
-        }
-        #endregion
-        #region Output
-        /// <summary>
-        /// The <see cref="Output" /> property's name.
-        /// </summary>
-        public const string OutputPropertyName = "Output";
-
-        private string _output;
-
-        /// <summary>
-        /// Sets and gets the Output property.
-        /// Changes to that property's value raise the PropertyChanged event. 
-        /// </summary>
-        public string Output
-        {
-            get
-            {
-                return _output;
-            }
-
-            set
-            {
-                if (_output == value)
-                {
-                    return;
-                }
-
-                RaisePropertyChanging(OutputPropertyName);
-                _output = value;
-                RaisePropertyChanged(OutputPropertyName);
-            }
-        }
-        #endregion
-        #endregion
     }
 }
--- Splitix/ViewModel/ViewModelLocator.cs
+++ Splitix/ViewModel/ViewModelLocator.cs
@@ -1,55 +1,50 @@
-﻿/*
+/*
   In App.xaml:
   <Application.Resources>
-      <vm:ViewModelLocatorTemplate xmlns:vm="clr-namespace:Splitix.ViewModel"
-                                   x:Key="Locator" />
+      <vm:ViewModelLocator xmlns:vm="clr-namespace:Splitix"
+                           x:Key="Locator" />
   </Application.Resources>
   
   In the View:
   DataContext="{Binding Source={StaticResource Locator}, Path=ViewModelName}"
+
+  You can also use Blend to do all this with the tool's support.
+  See http://www.galasoft.ch/mvvm
 */
 
 using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Ioc;
 using Microsoft.Practices.ServiceLocation;
-using Splitix.Model;
 
 namespace Splitix.ViewModel
 {
     /// <summary>
     /// This class contains static references to all the view models in the
     /// application and provides an entry point for the bindings.
-    /// <para>
-    /// See http://www.galasoft.ch/mvvm
-    /// </para>
     /// </summary>
     public class ViewModelLocator
     {
-        static ViewModelLocator()
+        /// <summary>
+        /// Initializes a new instance of the ViewModelLocator class.
+        /// </summary>
+        public ViewModelLocator()
         {
             ServiceLocator.SetLocatorProvider(() => SimpleIoc.Default);
 
-            SimpleIoc.Default.Register<Service.IInputParser, Service.InputParser>();
-
-            if (ViewModelBase.IsInDesignModeStatic)
-            {
-                SimpleIoc.Default.Register<IDataService, Design.DesignDataService>();
-            }
-            else
-            {
-                SimpleIoc.Default.Register<IDataService, DataService>();
-            }
+            ////if (ViewModelBase.IsInDesignModeStatic)
+            ////{
+            ////    // Create design time view services and models
+            ////    SimpleIoc.Default.Register<IDataService, DesignDataService>();
+            ////}
+            ////else
+            ////{
+            ////    // Create run time view services and models
+            ////    SimpleIoc.Default.Register<IDataService, DataService>();
+            ////}
 
             SimpleIoc.Default.Register<MainViewModel>();
-
         }
 
-        /// <summary>
-        /// Gets the Main property.
-        /// </summary>
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance",
-            "CA1822:MarkMembersAsStatic",
-            Justification = "This non-static member is needed for data binding purposes.")]
         public MainViewModel Main
         {
             get
@@ -57,12 +52,10 @@ namespace Splitix.ViewModel
                 return ServiceLocator.Current.GetInstance<MainViewModel>();
             }
         }
-
-        /// <summary>
-        /// Cleans up all the resources.
-        /// </summary>
+        
         public static void Cleanup()
         {
+            // TODO Clear the ViewModels
         }
     }
 }
